b908a77221989d2db1a8ac6fc85e1bcb31ad2f22
YokotaYoshi <ymdtr5274@gmail.com>
2025-03-21 00:56:54 +0900

    d

--- AlloyLaboratory/Assets/Scripts/FollowerController.cs
+++ AlloyLaboratory/Assets/Scripts/FollowerController.cs
@@ -11,11 +11,14 @@ public class FollowerController : MonoBehaviour
     float naiseki;
     float cosTheta;
     float theta;
-    bool isSameDirection = false;//プレイヤーの移動方向と一致しているかどうか
+    
+    bool isMoving = false;//動いているかどうか
     bool isCoroutineWorking = false;//コルーチン中かどうか
-    Vector2 targetPosition;//自動移動時のゴール
+    Vector2 Direction;//自動移動時のゴール
+    Vector2 targetPosition;//移動先
 
     float speed = 0.0f;//移動速度
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,7 +34,11 @@ public class FollowerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+
+        }
+        /*
         //キャラクターの移動先をプレイヤーの現在位置に設定
         if (player != null)
         {
@@ -60,22 +67,16 @@ public class FollowerController : MonoBehaviour
         else 
         {
             theta = 0;
-        }
+        }        
+        Debug.Log(Mathf.Rad2Deg*theta);
+        */
         
-        //Debug.Log(Mathf.Rad2Deg*theta);
-        if (theta < 0.1)
-        {
-            isSameDirection = true;
-        }
-        else
-        {
-            isSameDirection = false;
-        }
     }
 
     void FixedUpdate()
     {
-        if (plrb2d.linearVelocity.magnitude < 0.1f)
+        /*
+        if (plrb2d.linearVelocity.magnitude < 0.01f)
         {
             //プレイヤーの移動速度がほぼ0なら
             rb2d.linearVelocity = Vector2.zero;//こっちも0にする
@@ -86,28 +87,56 @@ public class FollowerController : MonoBehaviour
         {
             //プレイヤーが移動中なら
             //移動方向が一致しているなら
-            if (isSameDirection)
+            if (theta < 0.1f)
             {
 
                 rb2d.linearVelocity = plrb2d.linearVelocity;
             }
-            //移動方向が不一致ならコルーチン始動
-            if (!isSameDirection && !isCoroutineWorking)
+            //移動方向が交差ならコルーチン始動
+            else if (theta<3f)
             {
-                Debug.Log("開始");
-                StartCoroutine(Move());
+                if (!isCoroutineWorking)
+                {
+                    Debug.Log("開始");
+                    StartCoroutine(SideMove());
+                }
             }
-            
-        }
+            //移動方向が真逆なら
+            else
+            {
+                {
+                    //StartCoroutine(OppoMove());
+                }
+            }
+        }*/
     }
 
     IEnumerator Move()
     {
+        //動いているフラグ立て
+        isMoving = true;
+        float time = 0.0f;
+        targetPosition = new Vector2(Mathf.Round(player.transform.position.x), Mathf.Round(player.transform.position.y));
+        while()
+        {
+            yield return null;
+        }
+        //動いているフラグおろし
+        isMoving = false;
+        //座標を格子点に
+        transform.position = new Vector2(Mathf.Round(transform.position.x),Mathf.Round(transform.position.y));
+    }
+
+    IEnumerator SideMove()
+    {
         isCoroutineWorking = true;//コルーチン中
-        float time = playerPosition.magnitude/speed;//コルーチン時間計算
-        //コルーチン中の速度計算
-        rb2d.linearVelocity = new Vector2(speed * Mathf.Round(playerPosition.x), speed * Mathf.Round(playerPosition.y));
-        yield return new WaitForSeconds(time);//待機
+        //移動方向を設定
+        Direction = new Vector2(Mathf.Round(player.transform.position.x)-transform.position.x, Mathf.Round(player.transform.position.y)-transform.position.y);
+        //コルーチン時間計算
+        float time = Direction.magnitude/speed;
+        
+        
+        yield return new WaitForSeconds(time-0.01f);//待機
         //座標を格子点に
         transform.position = new Vector2(Mathf.Round(transform.position.x),Mathf.Round(transform.position.y));
         isCoroutineWorking = false;//コルーチン終わり
